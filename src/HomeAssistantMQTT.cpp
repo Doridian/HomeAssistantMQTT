d5fc246552ac143e30585929e3ed44e1764abd72
Mark Dietzer <git@doridian.net>
2022-03-08 12:28:44 -0800

    Initial

---
+++ src/HomeAssistantMQTT.cpp
@@ -0,0 +1,83 @@
+#include "HomeAssistantMQTT.h"
+
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+HomeAssistantMQTTDevice::HomeAssistantMQTTDevice(
+    const String &deviceType,
+    const String &name,
+    const String &id
+) {
+    this->name = name;
+    this->deviceType = deviceType;
+    if (id.isEmpty()) {
+        String _id = name;
+        _id.toLowerCase();
+        _id.replace(" ", "_");
+        this->id = _id;
+    } else {
+        this->id = id;
+    }
+    this->state = "";
+
+    this->mqttTopicBase = HA_BASE_TOPIC + this->deviceType + "/" + this->id;
+
+    this->configDoc["~"] = this->mqttTopicBase;
+    this->configDoc["name"] = this->name;
+    this->configDoc["unique_id"] = this->id;
+    this->configDoc["cmd_t"] = "~/" HA_COMMAND_SUFFIX;
+    this->configDoc["stat_t"] = "~/" HA_STATE_SUFFIX;
+    this->configDoc["json_attr_t"] = "~/" HA_ATTRIBUTES_SUFFIX;
+
+    this->refresh();
+}
+
+HomeAssistantMQTTDevice::~HomeAssistantMQTTDevice() {
+}
+
+void HomeAssistantMQTTDevice::refresh() {
+    this->configDirty = true;
+    this->stateDirty = true;
+    this->attributesDirty = true;
+}
+
+String HomeAssistantMQTTDevice::makeMQTTTopic(const String &topic) const {
+    return this->mqttTopicBase + "/" + topic;
+}
+
+String HomeAssistantMQTTDevice::makeMQTTCommandTopic() const {
+    return this->makeMQTTTopic(HA_COMMAND_SUFFIX);
+}
+
+void HomeAssistantMQTTDevice::loop(PubSubClient &mqtt) {
+    const unsigned long now = millis();
+    char buf[HA_JSON_MAX_SIZE];
+
+    if (this->configDirty) {
+        serializeJson(this->configDoc, buf);
+        mqtt.publish(this->makeMQTTTopic(HA_CONFIG_SUFFIX).c_str(), buf);
+        this->configDirty = false;
+        this->lastConfigUpdate = now;
+    }
+
+    if (now - this->lastConfigUpdate < HA_WAIT_BETWEEN_CONFIG_AND_DATA) {
+        return;
+    }
+
+    if (this->attributesDirty) {
+        serializeJson(this->attributesDoc, buf);
+        mqtt.publish(this->makeMQTTTopic(HA_ATTRIBUTES_SUFFIX).c_str(), buf);
+        this->attributesDirty = false;
+    }
+
+    if (this->stateDirty) {
+        mqtt.publish(this->makeMQTTTopic(HA_STATE_SUFFIX).c_str(), this->state.c_str());
+        this->stateDirty = false;
+    }
+}
+
+void HomeAssistantMQTTDevice::setState(const String &state) {
+    this->state = state;
+    this->stateDirty = true;
+}
---
+++ src/HomeAssistantMQTT.h
@@ -0,0 +1,66 @@
+#ifndef HOME_ASSISTANT_MQTT_H
+#define HOME_ASSISTANT_MQTT_H
+
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#define HA_JSON_MAX_SIZE 1024
+#define HA_WAIT_BETWEEN_CONFIG_AND_DATA 5000
+#define HA_BASE_TOPIC "homeassistant/"
+
+#define HA_STATE_SUFFIX "state"
+#define HA_ATTRIBUTES_SUFFIX "attr"
+#define HA_CONFIG_SUFFIX "config"
+#define HA_COMMAND_SUFFIX "cmd"
+#define HA_STATUS_TOPIC (HA_BASE_TOPIC "status")
+
+class HomeAssistantMQTTDevice
+{
+    public:
+        HomeAssistantMQTTDevice(
+            const String &deviceType,
+            const String &name,
+            const String &id = ""
+        );
+        ~HomeAssistantMQTTDevice();
+
+        void loop(PubSubClient &mqtt);
+        void command(const String &command);
+        void refresh();
+
+        void setState(const String &state);
+        
+        StaticJsonDocument<HA_JSON_MAX_SIZE> configDoc;
+        StaticJsonDocument<HA_JSON_MAX_SIZE> attributesDoc;
+
+        template <typename T>
+        void setAttribute(const String& name, const T& src) {
+            this->attributesDoc[name] = src;
+            this->attributesDirty = true;
+        }
+
+        template <typename T>
+        void setConfig(const String& name, const T& src) {
+            this->configDoc[name] = src;
+            this->configDirty = true;
+        }
+
+        String makeMQTTTopic(const String &topic) const;
+        String makeMQTTCommandTopic() const;
+
+    protected:
+        bool stateDirty;
+        bool attributesDirty;
+        bool configDirty;
+
+        String id;
+        String name;
+        String deviceType;
+        String mqttTopicBase;
+        String state;
+
+        unsigned long lastConfigUpdate;
+};
+
+#endif
